61ec197a8a1cea408f02b6ad4facfa507cdaf0be
Andy <and.razumov2014@gmail.com>
2023-04-01 15:02:04 +0800

    simple CDN server for microfrontends

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = 3030;
+const PUBLIC_DIR = path.resolve(__dirname, 'public')
+
+const app = express();
+
+app.use(cors());
+app.use(express.static(PUBLIC_DIR));
+
+app.get('/microfrontends', (_, res) => {
+  let dataToResponse = [];
+
+  if (fs.existsSync(PUBLIC_DIR)) {
+    const mfFolders = fs.readdirSync(PUBLIC_DIR);
+
+    for (const mfFolder of mfFolders) {
+      if (!fs.existsSync(path.resolve(PUBLIC_DIR, mfFolder, 'index.js'))) {
+        continue;
+      }
+
+      const mfInfo = {
+        name: mfFolder,
+        src: `/${mfFolder}/index.js`,
+        route: `/${mfFolder}`,
+      }
+
+      if (fs.existsSync(path.resolve(PUBLIC_DIR, mfFolder, 'index.css'))) {
+        mfInfo.styles = `/${mfFolder}/index.css`;
+      }
+
+      dataToResponse.push(mfInfo);
+    }
+  }
+
+  res.json(dataToResponse);
+});
+
+console.log("Это тестовый сервер, выполняющий роль CDN");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
